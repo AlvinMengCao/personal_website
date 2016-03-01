cade46687ba1580c6154ebd1a1616c80460d0a79
alvin <mengcao.cm@gmail.com>
2016-02-29 21:14:56 -0500

    button

--- src/app/job/blogs.js
+++ src/app/job/blogs.js
@@ -3,14 +3,40 @@
  */
 angular.module('job',[])
     .controller('BlogController',function($http){
-        var blog =this;
-        blog.lists=[];
+        var blog = this;
+        blog.lists = [];
+        blog.comments = [];
+        blog.blog_number = 0;
+        blog.comment_number = 0;
+        blog.name = "";
+        blog.email = "";
+        blog.comment = "";
 
         this.load = function(){
             $http.get("http://alvin-api.herokuapp.com/application/blogs")
                 .success(function(data){
-                    blog.lists=data;
+                    blog.lists = data;
+                    blog.blog_number = Object.keys(data).length;
                 });
+
+            $http.get("http://alvin-api.herokuapp.com/application/website-comments")
+                .success(function(data){
+                    blog.comments = data;
+                    blog.comment_number = Object.keys(data).length;
+                });
+
+
+        };
+        this.addComment = function(){
+
+            $http.post("http://alvin-api.herokuapp.com/application/website-comments?" +
+                "email=" + blog.email + "&comment=" + blog.comment + "&name=" + blog.name,
+                {}
+            ).then(function(response){
+                alert("success");
+            });
+
+
         };
 
 
