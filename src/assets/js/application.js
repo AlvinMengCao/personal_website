fcc37c99cc32ebc65ffc55a36b50fa596261cf52
alvin <mengcao.cm@gmail.com>
2016-03-03 20:49:02 -0500

    add resume

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -3,12 +3,14 @@ angular.module('application',['ngRoute', 'job'])
     .config(['$routeProvider',function($routeProvider){
         $routeProvider
             .when('/',{
-               templateUrl: 'src/app/other/homepage.html'
+               templateUrl: 'src/app/other/homepage.html',
+
             })
             .when('/:category/:page',{
                 templateUrl: function(params) {
                                 return 'src/app/'+params.category+'/'+params.page+'.html';
-                }
+                },
+
             })
             .otherwise({
                 redirectTo: '/'
