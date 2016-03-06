91e99eba4c4b59b3984ff39a0714790f70a6c7a7
alvin <mengcao.cm@gmail.com>
2016-03-05 21:35:56 -0500

    add first skill group

--- src/app/job/skills.js
+++ src/app/job/skills.js
@@ -1,4 +1,13 @@
 angular.module('skill',[])
-    .controller('SkillController', function($route){
+    .controller('SkillController', function($location, $anchorScroll){
+
+        this.tab = 1;
+        this.setTab = function (tabId) {
+            this.tab = tabId;
+        };
+
+        this.isSet = function (tabId) {
+            return this.tab === tabId;
+        };
 
     });
